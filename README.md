338292bc51498fd7430dfffcb9b9da84dba7a7c3
Ubuntu <ubuntu@ip-172-31-62-216.ec2.internal>
2018-11-05 14:54:56 +0000

    Adapted main.py to use lstm model. Updated requirements with fastai's dependencies and zappa created zappa_settings

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Basic structure to deploy an AI project with flask
 ## Structure of Repository
 
 * install_all.sh : Script to install all dependencies
-* config.static.json: rename to config.json and put any sensitive configuration there
 * main.py: Contains all the logic
 * models/*: Contains all the trained models
 
--- main.py
+++ main.py
@@ -6,10 +6,14 @@ import json
 import pickle
 import os
 import requests
+from fastai.text import get_language_model
+import numpy as np
+import torch 
 
 app = Flask(__name__)
 
-MODEL_PATH= 'models/'
+MODEL_PATH= './models/awd_lstm/lstm_wt103.pth'
+ITOS_PATH = './models/awd_lstm/itos_wt103.pkl'
 
 def pickle_obj(data, path):
     with open(path,'wb+') as f:
@@ -19,25 +23,80 @@ def unpickle_obj(path):
     with open(path,'rb') as f:
         data = pickle.load(f)
     return data
+ 
+def sample_model(model, input_words, l=50):
+    '''
+    model: pytorch LM 
+    s: list of strings
+    '''
+    no_space_words = ["'s", "'ll", ",", "?",".", "'t", "'m", "n't", "!", "'", "'ve", ";"]
+    capitalize_words = ['.', '!', '\n']
+    exclude_tokens = [model.stoi[i] for i in ['"',"[", "]", "(", ")", "xxup", "xxfld", "xxrep", "xxbos"] if i in model.stoi]
+    bs = model[0].bs
+    model[0].bs=1
+    model.eval()
+    model.reset()
+    final_string = ''
+    # Gives the model the input strings
+    for s in input_words:
+        t = model.stoi[s] if s in model.stoi else 0 
+        res,*_ = model.forward(torch.tensor([[t]]).cpu())
+        final_string = final_string + ' ' + s
+    last_word = None
+    # predicts l number of next words
+    for i in range(l):
+        result_indexes = torch.multinomial(res[-1].exp(), 10)
+        # selects a word that is not in the exclude_tokens list
+        for r in result_indexes:
+            if r != 0 and r not in exclude_tokens: 
+                word_index = r
+                break
+            else:
+                word_index = result_indexes[0]
+        
+        word = model.itos[word_index]
+        res, *_ = model.forward(torch.tensor([[word_index.tolist()]]).cpu())
+        # Capitalize if it is the last word in a phrase
+        word = word.capitalize() if last_word in  capitalize_words else word
+        # Do not place a space in front of the words in no_space_words
+        if word in no_space_words:
+            final_string = final_string + word
+        else: 
+            final_string = final_string + ' ' + word
+        last_word = word
+    model[0].bs=bs
+    return final_string
     
-def load_and_predict(input_data, model_path):
+def load_lm_and_predict():
     ''' input_data: data to be used in the prediction
     model_path: path to the 
     '''
-    return {'text': ""}
+    #Unpickles list representing vocabulary
+    itos = unpickle_obj(ITOS_PATH)
+    # Generates dictionary mapping token to int
+    stoi = {i[1]:i[0] for i in enumerate(itos)}
+    # Generates AWD_LSTM model
+    dps = np.array([0.25, 0.1, 0.2, 0.02, 0.15]) * 1.0
+    my_model = get_language_model(vocab_sz=len(itos), emb_sz=400, n_hid=1150, n_layers=3, pad_token=1, input_p=dps[0],                    output_p=dps[1],weight_p=dps[2], embed_p=dps[3], hidden_p=dps[4], tie_weights=True, bias=True, qrnn=False)
+    # load all the weights in the model
+    my_model.load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
+    my_model.itos = itos
+    my_model.stoi = stoi
+    #print(my_model)
+    return {'text': sample_model(my_model, [''], l=50)}
 
-@app.route('/inference',methods=['POST'])
+@app.route('/inference',methods=['GET'])
 def inference():
     ''' 
-    POST: Receives some parameters on the post request and makes an inference
-    using the trained model
+    GET: Performs inference on the language model
     '''
-    form_data = flask.request.get_json()
-    data = form_data['data']
-    response = search_doc(data, model_path = MODEL_PATH)
+    #form_data = flask.request.get_json()
+    #data = form_data['data']
+    response = load_lm_and_predict()
     resp = Response(response=json.dumps({"response": response}), status=200, mimetype='application/json')
     return resp
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8082)
+    # print(load_lm_and_predict())
