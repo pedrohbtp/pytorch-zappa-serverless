3d313b5f00e2f7fcc62bcef5cd461fbf49131cf4
Ubuntu <ubuntu@ip-172-31-62-216.ec2.internal>
2018-11-05 15:01:09 +0000

    Downloads example awd_lstm LM in install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,4 +1,8 @@
 python3.6 -m venv python3.6_venv
 source python3.6_venv/bin/activate
 pip3 install -r requirements.txt
+# Download example models
 mkdir models
+mkdir aws_lstm
+wget http://files.fast.ai/models/wt103_v1/lstm_wt103.pth -P ./models/awd_lstm
+wget http://files.fast.ai/models/wt103_v1/itos_wt103.pkl -P ./models/awd_lstm
